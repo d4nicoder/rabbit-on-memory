f1f6299722565154fbbf561c9eea563ee9fb73b3
Daniel Cañada Garcia <danitetus@gmail.com>
2021-05-16 19:24:41 +0200

    fix: update build files

--- lib/RabbitInMemory.d.ts
+++ lib/RabbitInMemory.d.ts
@@ -1,12 +1,16 @@
 import { IQueueBinding } from "./interfaces/IQueueBinding";
+import { IConfigOptions } from "./interfaces/IConfigOptions";
 import { IPublishOptions } from "./interfaces/IPublishOptions";
 export default class RabbitOnMemory {
     static instance: RabbitOnMemory;
     private readonly exchanges;
     private readonly routes;
     private consumerTag;
+    private configuration;
     private constructor();
     private setExchange;
+    private runQueuesAsync;
+    private runQueuesSync;
     private runQueues;
     private processQueuesFanout;
     private processQueuesDirect;
@@ -14,5 +18,6 @@ export default class RabbitOnMemory {
     static getInstance(): RabbitOnMemory;
     bindQueue(options: IQueueBinding): void;
     publishRoute(options: IPublishOptions): Promise<void>;
+    setConfig(options: IConfigOptions): void;
 }
 //# sourceMappingURL=RabbitInMemory.d.ts.map
--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,8 +1,10 @@
+import { IConfigOptions } from "./interfaces/IConfigOptions";
 import { IPublishOptions } from "./interfaces/IPublishOptions";
 import { IQueueBinding } from "./interfaces/IQueueBinding";
 declare const _default: {
     publish: (options: IPublishOptions) => Promise<void>;
-    subscribe: <T>(options: IQueueBinding) => void;
+    subscribe: (options: IQueueBinding) => void;
+    setConfig: (options: IConfigOptions) => void;
 };
 export default _default;
 //# sourceMappingURL=index.d.ts.map
