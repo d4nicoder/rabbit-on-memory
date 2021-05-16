c8d1c1a444af9263eda0420657973c23245b7c18
Daniel Cañada Garcia <danitetus@gmail.com>
2021-05-16 16:38:52 +0200

    doc: added jsdoc

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,22 @@ import { IPublishOptions } from "./interfaces/IPublishOptions";
 import { IQueueBinding } from "./interfaces/IQueueBinding";
 import RabbitOnMemory from "./RabbitInMemory";
 
+/**
+ * Publish a message in an exchange route. All the subscriber with valid binding keys
+ * will be called with the payload.
+ * @param options Publish options
+ * @returns A promise when all the subscriber have ended
+ */
 const publish = async (options: IPublishOptions): Promise<void> => {
   const rabbitInstance = RabbitOnMemory.getInstance()
   return rabbitInstance.publishRoute(options)
 }
 
+/**
+ * Creates a queue binded to a route in one exchange. It is lazy created, so if
+ * queue not exists, it will be created in this moment
+ * @param options Subscribing options
+ */
 const subscribe = <T>(options: IQueueBinding): void => {
   const rabbitInstance = RabbitOnMemory.getInstance()
   rabbitInstance.bindQueue(options)
